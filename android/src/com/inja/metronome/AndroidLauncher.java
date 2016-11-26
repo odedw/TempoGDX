e8eae0c09d36a4be3fb71f8195eb28a50a98387b
Oded Welgreen <oded.welgreen@gmail.com>
2016-11-26 16:05:11 +0200

    initial commit

---
+++ android/src/com/inja/metronome/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.inja.metronome;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.inja.metronome.MetronomeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MetronomeGame(), config);
+	}
+}
